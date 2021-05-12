d12fbe9135cafb216532305cc7f0e992ba761cf6
sarinali <74844953+sarinali@users.noreply.github.com>
2021-05-12 15:18:31 -0400

    pause works!

---
+++ sampleProject/Assets/Scripts/pausebuttonusual.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausebuttonusual : MonoBehaviour{
+    
+    public GameObject usualButton;
+    // Start is called before the first frame update
+    void Start(){
+        usualButton.SetActive(true);
+    }
+    public void pressed(){
+        usualButton.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
--- sampleProject/Assets/Scripts/pausecomp.cs
+++ sampleProject/Assets/Scripts/pausecomp.cs
@@ -2,36 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pausecomp : MonoBehaviour
-{
-    // public static bool paused = false; 
+public class pausecomp : MonoBehaviour {
     public GameObject pauseMenuUI;
-    // void Setup() {
-    //     pauseMenuUI.SetActive(true);
-    // }
-    // Update is called once per frame
-    // void Update(){
-    //     pauseMenuUI.SetActive(false);
-    //     Time.timeScale = 1f;
-    //     paused = false;
-    // }
-    // public void pause() {
-    //     pauseMenuUI.SetActive(true);
-    //     Time.timeScale = 0f;
-    //     paused = true;
-    // }
+    public GameObject usualButton;
+    public void Setup() {
+        // normal screen on, pause screen off
+        usualButton.SetActive(true);
+        pauseMenuUI.SetActive(false);
+    }
+    public void pausegame() {
+        // usual off, pause on
+        pauseMenuUI.SetActive(true);
+        usualButton.SetActive(false);
+        Time.timeScale = 0f;
+    }
     public void resume() {
         // if u press the back ig
         pauseMenuUI.SetActive(false);
+        usualButton.SetActive(true);
         Time.timeScale = 1f;
     }
     public void privacyPolicy() {
-
+        // add another canvas for this later
     }
     public void credits(){
-
+        // add another canvas for credits
     }
     public void about() {
-
+        // add something for about as well 
     }
 }
