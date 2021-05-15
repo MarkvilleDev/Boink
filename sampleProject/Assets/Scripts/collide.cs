710bf7849c9f788f8dfe35d0ac87ff8fa2982ea9
dourian <75771131+dourian@users.noreply.github.com>
2021-05-14 22:21:41 -0400

    Update collide.cs

--- sampleProject/Assets/Scripts/collide.cs
+++ sampleProject/Assets/Scripts/collide.cs
@@ -28,6 +28,6 @@ public levelchanger levelchangescript;
     }
     private void win()
     {
-        levelchangescript.fadeToLevel(SceneManager.GetActiveScene().buildIndex);
+        //levelchangescript.fadeToLevel(SceneManager.GetActiveScene().buildIndex);
     }
 }
