4143373ad375ced5fc885433c0043dbc67595814
dourian <75771131+dourian@users.noreply.github.com>
2021-06-01 00:54:22 -0400

    ok

--- sampleProject/Assets/Scripts/skinSelector.cs
+++ sampleProject/Assets/Scripts/skinSelector.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class levelSelector : MonoBehaviour
+public class skinSelector : MonoBehaviour
 {
     public Image[] lockImages;
-    public Button[] lvlButtons;
+    public Button[] skinImage;
     public GameObject load;
-    
+
+
     void Start()
     {
-        load.GetComponent<Player>().SavePlayer();
-
         for (int i = 1; i < lockImages.Length; i++)
         {
-            lvlButtons[i].interactable = false;
+            skinImage[i].interactable = false;
         }
         load.GetComponent<Player>().LoadPlayer();
-        for (int i = 0; i < lockImages.Length; i++) {
-            if (load.GetComponent<Player>().levels[i]) {
+        for (int i = 0; i < lockImages.Length; i++)
+        {
+            if (load.GetComponent<Player>().levels[i])
+            {
                 lockImages[i].enabled = false;
-                lvlButtons[i].interactable = true;
+                skinImage[i].interactable = true;
             }
         }
         load.GetComponent<Player>().SavePlayer();
